b618b028abcedaea9daf33594ddcd1570b4c7318
Juan Esteban <juanestban000@gmail.com>
2022-09-25 23:42:56 -0500

    fix: error to try to use petition without tokens

--- config/Wrapper.tsx
+++ config/Wrapper.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, ReactNode } from 'react';
 // import { useRouter } from 'next/router';
-import { useNoteContext } from '@lilith/contexts';
+import { useNoteContext, useSession } from '@lilith/contexts';
 
 import { Input, Button } from '@lilith/components';
 
@@ -9,7 +9,9 @@ interface WrapperProps {
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const { user } = useSession();
   const { noteForm, handleAdd, handleChange } = useNoteContext();
+  const { token } = user;
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -18,15 +20,17 @@ export const Wrapper = ({ children }: WrapperProps) => {
 
   return (
     <>
-      <header role="navigation">
-        <div className="container-form-input">
-          <form onSubmit={handleSubmit}>
-            {/* resolve error with this input */}
-            <Input type="text" className="input-note" placeholder="title of note" value={noteForm} onInput={handleChange} />
-            <Button className="button-note-save">save note</Button>
-          </form>
-        </div>
-      </header>
+      {token && (
+        <header role="navigation">
+          <div className="container-form-input">
+            <form onSubmit={handleSubmit}>
+              {/* resolve error with this input */}
+              <Input type="text" className="input-note" placeholder="title of note" value={noteForm} onInput={handleChange} />
+              <Button className="button-note-save">save note</Button>
+            </form>
+          </div>
+        </header>
+      )}
       {children}
     </>
   );
