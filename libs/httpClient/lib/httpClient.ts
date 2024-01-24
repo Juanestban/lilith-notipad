2986c083aa0362216435c63ba27a825f5e46416e
Juan Esteban <juanestban000@gmail.com>
2024-01-24 15:39:54 -0500

    fix: errors to request api

--- libs/httpClient/lib/httpClient.ts
+++ libs/httpClient/lib/httpClient.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const httpClient: AxiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '/lilith-notipad/api' : '/api',
-  timeout: process.env.NODE_ENV === 'production' ? 60000 : 180000,
+  baseURL: process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS ? '/lilith-notipad/api' : '/api',
+  timeout: process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS ? 60000 : 180000,
 });
 
 export default httpClient;
