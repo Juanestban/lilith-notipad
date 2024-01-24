7d7313d3975b5ee5ed1ef61da010e0e5c3ae2851
Juan Esteban <juanestban000@gmail.com>
2024-01-24 15:02:33 -0500

    fix: creating wrapper for handle envs on production

--- next.config.js
+++ next.config.js
@@ -4,11 +4,16 @@ env.config();
 
 require('./config/mongoose');
 
+const isProduction = process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS
+const basePath = isProduction ? '/lilith-notipad' : '/'
+const assetPrefix = isProduction ? '/lilith-notipad/' : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/lilith-notipad',
+  basePath,
+  assetPrefix,
   env: {
     MONGODB_ATLAS_USERNAME: process.env.MONGODB_ATLAS_USERNAME,
     MONGODB_ATLAS_PASSWORD: process.env.MONGODB_ATLAS_PASSWORD,
