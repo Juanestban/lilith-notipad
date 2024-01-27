4be0e6a0f2a92659fa906b15b690cc61975ceffe
Juan Esteban <juanestban000@gmail.com>
2024-01-26 23:19:25 -0500

    fix: removed other config next prop, missing upgrade next packages

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,10 @@ import connect from './config/mongoose.mjs';
 env.config();
 connect();
 
-const isProduction = process.env.NODE_ENV === 'production' || process.env.GITHUB_ACTIONS;
-const basePath = isProduction ? '/lilith-notipad' : '/';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath,
-  assetPrefix: './',
   env: {
     MONGODB_ATLAS_USERNAME: process.env.MONGODB_ATLAS_USERNAME,
     MONGODB_ATLAS_PASSWORD: process.env.MONGODB_ATLAS_PASSWORD,
