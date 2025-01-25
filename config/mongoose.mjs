ed5636b0b6b817a229d44829b0edf23731286150
Juan Esteban <juanestban000@gmail.com>
2025-01-25 00:38:37 -0500

    fix: error to have multiple connections

--- config/mongoose.mjs
+++ config/mongoose.mjs
@@ -4,9 +4,10 @@ const { MONGODB_ATLAS_USERNAME, MONGODB_ATLAS_PASSWORD, NODE_ENV } = process.env
 const database = NODE_ENV === 'production' ? 'notipadDB' : 'notipadDB_development';
 const isProduction = NODE_ENV === 'production';
 const urlMongo = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@lilith-notipad.qzy3ydb.mongodb.net/${database}?retryWrites=true&w=majority`;
+const uri = isProduction ? process.env.MONGODB_URI : urlMongo;
 
 mongoose
-  .connect(isProduction ? process.env.MONGODB_URI : urlMongo, {
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -15,7 +16,7 @@ mongoose
 
 const connect = () => {
   mongoose
-    .connect(urlMongo, {
+    .connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
