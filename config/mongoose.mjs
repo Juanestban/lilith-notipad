4bc11429ab91faf3fb7c38c23735c4698f8681ec
Juan Esteban Perdomo <53704567+Juanestban@users.noreply.github.com>
2026-06-14 23:18:04 -0500

    fix: using all in productions

--- config/mongoose.mjs
+++ config/mongoose.mjs
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
-const database = NODE_ENV === 'production' ? 'notipadDB' : 'notipadDB_development';
+// const database = NODE_ENV === 'production' ? 'notipadDB' : 'notipadDB_development';
+const database = 'notipadDB';
 const urlMongo = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@lilith-notipad.qzy3ydb.mongodb.net/appName=${database}?retryWrites=true&w=majority`;
 
 const connect = () => {
   mongoose
-    .connect(NODE_ENV === 'production' ? MONGODB_URI : urlMongo, {
+    .connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
