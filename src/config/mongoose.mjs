e93163b781c54a3d32be288ffc860f17074e69bb
Juan Esteban Perdomo <53704567+Juanestban@users.noreply.github.com>
2026-06-14 22:53:30 -0500

    using a new env mongodb_uri

--- src/config/mongoose.mjs
+++ src/config/mongoose.mjs
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-const { MONGODB_ATLAS_USERNAME, MONGODB_ATLAS_PASSWORD, NODE_ENV } = process.env;
+const MONGODB_URI = process.env.MONGODB_URI;
 const database = NODE_ENV === 'production' ? 'notipadDB' : 'notipadDB_development';
-const urlMongo = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@lilith-notipad.qzy3ydb.mongodb.net/${database}?retryWrites=true&w=majority`;
+const urlMongo = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@lilith-notipad.qzy3ydb.mongodb.net/appName=${database}?retryWrites=true&w=majority`;
 
 const connect = () => {
   mongoose
-    .connect(urlMongo, {
+    .connect(NODE_ENV === 'production' ? MONGODB_URI : urlMongo, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
